60904ac03d5eced4609e7f1b2fb5149c46d00b47
Steve Fokas <sf487552@gmail.com>
2021-10-14 16:20:24 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+$('.navBtn').click(function() {
+  $('.display').hide();
+  $(this.getAttribute('href')).show()
+});
+
+$('.homeBtn').click(function() {
+  $('.display').hide();
+  $(this.getAttribute('href')).show()
+});
+
+$('.closeBtn').click(function() {
+  $('#Sidebar').hide();
+});
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("Sidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("Sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+// slideshow
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow(slidePosition += n);
+}
+
+//  images controls
+function currentSlide(n) {
+  SlideShow(slidePosition = n);
+}
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  var circles = document.getElementsByClassName("dots");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+      circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePosition-1].style.display = "block";
+  circles[slidePosition-1].className += " enable";
+} 
+
+
+
